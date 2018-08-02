81cd680cadf91a3fd384ea0cf233bbc32f37c43d
herijaona3 <herijaona3@gmail.com>
2018-08-02 10:11:04 +0200

    first

---
+++ include/require.php
@@ -0,0 +1,35 @@
+<?php
+session_start ();
+$errors = [];
+$identity = array('technicien','chefservice','chefetablissement','corporate');
+$mdp ='motdepasse';
+if (isset($_POST['submit']) && (!empty($_POST['submit']))) {
+      if(!in_array(($_POST['identity']), $identity, true) || ($_POST['mdp']) != $mdp){
+        if (empty($_POST['identity']) && empty($_POST['mdp'])){
+            $errors['identity'] = 'Identifiant ou mot de passe vide!';
+        }else{
+        $errors['identity'] = 'Identifiant ou mot de passe incorrect!';
+        }
+      }   
+      if(($_POST['mdp']) == $mdp){
+          if(($_POST['identity']) == $identity['0']){
+                header("Location:technicien.php");   
+                exit; 
+            }
+            if(($_POST['identity']) == $identity['1']){
+                header("Location:service.php");   
+                exit; 
+            }
+            if(($_POST['identity']) == $identity['2']){
+                header("Location:etablissement.php");   
+                exit; 
+            }
+            if(($_POST['identity']) == $identity['3']){
+                header("Location:corporate.php");   
+                exit; 
+            }
+      }
+}
+
+?>
+
---
+++ index.php
@@ -0,0 +1,38 @@
+
+<?php
+    include "include/require.php"; 
+    include "tpl/header.php"; 
+?>
+    <div class="header">
+        <div class="container">
+            <div class="logo">
+                <h1>
+                    <a href="#">Cronos</a>
+                </h1>
+            </div>
+            <div class="date">
+               Le <?php echo date("d/m/Y");  ?>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">
+            <p class="welcome-portal">Bienvenu sur Le Portail Cronos</p>
+            <?php if($errors) { ?>
+                <div class="alert alert-danger">
+                    <?php
+                        foreach ($errors as $value){ echo $value;} 
+                        unset($value);
+                    ?>
+                </div> 
+            <?php } ?>
+            <form action="index.php" method="post" class="form-login">
+                <input name="identity" type="text" class="form-control" placeholder="Identifiant">
+                <input name="mdp" type="password" class="form-control" placeholder="Mot de passe">
+                <input type="submit" name="submit" value="OK" class="submit-ok">
+            </form>
+        <div>
+    </div>
+
+<?php include "tpl/footer.php"; ?>
+    
